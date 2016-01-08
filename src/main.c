dc5b21c07cdd45954f1ac9fd08fa52e3cb15cef1
Kyle Blake <kyle@klkblake.com>
2016-01-09 01:54:27 +1030

    Add UTF-8, error reporting, rethink tokens

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include "xid_start_table.gen.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,10 +13,14 @@ typedef struct {
 } Location;
 
 typedef enum {
-	TOK_WORD,
-	TOK_OPERATOR,
+	TOK_LETTERS,
+	TOK_DIGITS,
+	TOK_SYMBOL,
 	TOK_BRACKET,
 	TOK_STRING,
+	TOK_NEWLINE,
+	TOK_WHITESPACE,
+	TOK_COMMENT,
 	TOK_UNKNOWN,
 	TOK_EOF,
 } TokenType;
@@ -24,10 +30,36 @@ typedef struct {
 	Location location;
 	u8 *start;
 	u32 len;
-	s32 indent_level;
-	b1 has_alignment;
 	char *str;
 } Token;
+
+typedef enum {
+	LEX_ERROR_UTF8_UNEXPECTED_CONTINUATION_CHAR,
+	LEX_ERROR_UTF8_OVERLONG_SEQUENCE,
+	LEX_ERROR_UTF8_EOF_IN_SEQUENCE,
+	LEX_ERROR_UTF8_SEQUENCE_TOO_SHORT,
+	LEX_ERROR_UTF8_CODE_POINT_TOO_HIGH,
+	LEX_ERROR_EOF_IN_COMMENT,
+	LEX_ERROR_EOF_IN_STRING,
+	LEX_ERROR_UNTERMINATED_STRING,
+} LexerErrorType;
+
+internal char *lexer_error_messages[] = {
+	"Invalid UTF-8 encoding: unexpected continuation byte",
+	"Invalid UTF-8 encoding: sequence too long (> 4 bytes)",
+	"Invalid UTF-8 encoding: hit EOF while decoding sequence",
+	"Invalid UTF-8 encoding: too few of continuation bytes in sequence",
+	"Invalid UTF-8 encoding: code point exceeded limit of 0x10ffff",
+	"Hit EOF while looking for end of comment",
+	"Hit EOF while looking for end of string",
+	"String was not terminated before end of line",
+};
+
+typedef struct {
+	LexerErrorType type;
+	Location location_start;
+	Location location_end;
+} LexerError;
 
 typedef struct {
 	u8 *file;
@@ -35,8 +67,29 @@ typedef struct {
 	u32 len;
 	b32 last_not_newline;
 	b32 in_indent;
+	u8 **lines;
 	Location location;
+	LexerError *errors;
+	u32 error_count;
+	u32 error_cap;
 } LexerState;
+
+internal
+void report_error_single_line(LexerState *state, LexerErrorType type, Location location, u32 column_end) {
+	if (!state->errors) {
+		state->error_cap = 8;
+		state->errors = malloc(state->error_cap * sizeof(LexerError));
+	}
+	if (state->error_count == state->error_cap) {
+		state->error_cap += state->error_cap >> 1;
+		state->errors = realloc(state->errors, state->error_cap);
+	}
+	LexerError *error = &state->errors[state->error_count++];
+	error->type = type;
+	error->location_start = location;
+	error->location_end = location;
+	error->location_end.column = column_end;
+}
 
 internal
 s32 peek(LexerState *state) {
@@ -53,34 +106,98 @@ s32 next_char(LexerState *state) {
 	} else {
 		state->location.line++;
 		state->location.column = 1;
+		state->lines[state->location.line - 1] = state->file + state->index;
 		state->in_indent = true;
 	}
 	if (state->index == state->len) {
 		return -1;
 	}
+retry:;
 	s32 c = state->file[state->index++];
-	if ((c & 0x80) != 0) {
-		// TODO check for underflow?
-		state->location.column--;
+	if (c == 0xff) {
+		report_error_single_line(state, LEX_ERROR_UTF8_OVERLONG_SEQUENCE,
+		                         state->location, state->location.column);
+		goto retry;
+	}
+	u32 count = (u32)__builtin_clz((u32)c ^ 0xff) - 24;
+	if (count == 1) {
+		report_error_single_line(state, LEX_ERROR_UTF8_UNEXPECTED_CONTINUATION_CHAR,
+		                         state->location, state->location.column);
+		goto retry;
+	} else if (count > 4) {
+		report_error_single_line(state, LEX_ERROR_UTF8_OVERLONG_SEQUENCE,
+		                         state->location, state->location.column);
+	} else if (count != 0) {
+		u8 chars[count];
+		chars[0] = (u8)c;
+		chars[0] <<= count;
+		chars[0] >>= count;
+		for (u32 i = 1; i < count; i++) {
+			if (state->index == state->len) {
+				report_error_single_line(state, LEX_ERROR_UTF8_EOF_IN_SEQUENCE,
+				                         state->location, state->location.column);
+				return -1;
+			}
+			chars[i] = state->file[state->index++];
+			if ((chars[i] >> 6) != 2) {
+				report_error_single_line(state, LEX_ERROR_UTF8_SEQUENCE_TOO_SHORT,
+				                         state->location, state->location.column);
+				goto retry;
+			}
+			chars[i] &= 0x7f;
+		}
+		c = chars[0] << (count - 1) * 6;
+		for (u32 i = 1; i < count; i++) {
+			c |= chars[i] << (count - i - 1) * 6;
+		}
+		if (c > 0x10ffff) {
+			report_error_single_line(state, LEX_ERROR_UTF8_CODE_POINT_TOO_HIGH,
+			                         state->location, state->location.column);
+		}
 	}
 	state->last_not_newline = c != '\n';
 	return c;
 }
 
-// TODO UTF-8.
 internal
-b32 is_word(s32 c) {
-	// TODO when we have unicode, include subscript 10.
-	return ('a' <= c && c <= 'z' ||
-	        'A' <= c && c <= 'Z' ||
-	        '0' <= c && c <= '9' ||
-	        c == '_' ||
-	        c == '#');
+b32 is_letter(s32 ch) {
+	if (ch == -1) {
+		return false;
+	}
+	u32 c = (u32)ch;
+	// TODO uncomment after testing binary search
+	/*
+	if (c <= 0xff) {
+		for (u32 i = 0; i < array_count(xid_start_table); i++) {
+			UnicodeRange range = xid_start_table[i];
+			if (range.start > c) {
+				return false;
+			}
+			if (range.start <= c && c <= range.end) {
+				return true;
+			}
+		}
+		return false;
+	}
+	*/
+	u32 lo = 0;
+	u32 hi = array_count(xid_start_table) - 1;
+	while (lo < hi) {
+		u32 mid = (lo + hi) / 2;
+		if (xid_start_table[mid].end < c) {
+			lo = mid + 1;
+		} else {
+			hi = mid;
+		}
+	}
+	UnicodeRange range = xid_start_table[lo];
+	return range.start <= c && c <= range.end;
 }
 
 internal
-b32 is_operator(s32 c) {
+b32 is_symbol(s32 c) {
 	return (c == '!' ||
+	        c == '#' ||
 	        c == '$' ||
 	        c == '%' ||
 	        c == '&' ||
@@ -99,6 +216,7 @@ b32 is_operator(s32 c) {
 	        c == '@' ||
 	        c == '\\' ||
 	        c == '^' ||
+	        c == '_' ||
 	        c == '`' ||
 	        c == '|' ||
 	        c == '~');
@@ -114,33 +232,12 @@ b32 is_bracket(s32 c) {
 internal
 void next_token(LexerState *state, Token *token) {
 	s32 c;
-	// TODO comments!
-	do {
-		c = next_char(state);
-		if (state->in_indent) {
-			token->indent_level = 0;
-			while (c == '\t') {
-				token->indent_level++;
-				c = next_char(state);
-			}
-			token->has_alignment = c == ' ';
-			state->in_indent = false;
-		} else {
-			token->indent_level = -1;
-		}
-		if (c == -1) {
-			token->type = TOK_EOF;
-			token->location = state->location;
-			return;
-		}
-		while (c == ' ') {
-			c = next_char(state);
-			// TODO eat comments here?
-		}
-	} while (c == '\n');
-	token->location = state->location;
-	token->start = state->file + state->index - 1;
-	token->len = 1;
+	c = next_char(state);
+	if (c == -1) {
+		token->type = TOK_EOF;
+		token->location = state->location;
+		return;
+	}
 #define ADD_WHILE(expr) \
 	c = peek(state); \
 	while (expr) { \
@@ -148,21 +245,112 @@ void next_token(LexerState *state, Token *token) {
 		token->len++; \
 		c = peek(state); \
 	}
-	if (is_word(c)) {
-		token->type = TOK_WORD;
-		ADD_WHILE(is_word(c));
-	} else if (is_operator(c)) {
-		token->type = TOK_OPERATOR;
-		ADD_WHILE(is_operator(c));
-	} else if (is_bracket(c)) {
-		token->type = TOK_BRACKET;
-	} else if (c == '"') {
-		// TODO handle escapes, figure out multiline strategy
-		token->type = TOK_STRING;
-		ADD_WHILE(c != '"' && c != '\n' && c != -1);
-	} else {
-		token->type = TOK_UNKNOWN;
+	// TODO state->file is in bytes, not codepoints
+	token->start = state->file + state->index - 1;
+	token->len = 1;
+	if (c == '\n') {
+		token->type = TOK_NEWLINE;
+		ADD_WHILE(c == '\t');
+		token->location = state->location;
+		if (token->len > 1) {
+			token->location.column = 1;
+		}
+		return;
 	}
+	token->location = state->location;
+	if (c == ' ') {
+		token->type = TOK_WHITESPACE;
+		ADD_WHILE(c == ' ');
+		return;
+	}
+	if (c == '/') {
+		s32 next = peek(state);
+		if (next == '/') {
+			token->type = TOK_COMMENT;
+			ADD_WHILE(c != '\n' && c != -1);
+			return;
+		} else if (next == '*') {
+			token->type = TOK_COMMENT;
+			next_char(state);
+			token->len++;
+			u32 depth = 1;
+			while (depth > 0) {
+				c = peek(state);
+				if (c == -1) {
+					report_error_single_line(state, LEX_ERROR_EOF_IN_COMMENT,
+					                         token->location, token->location.column + 1);
+					return;
+				}
+				next_char(state);
+				token->len++;
+				// TODO handle EOF
+				if (c == '/') {
+					next = peek(state);
+					if (next == '*') {
+						next_char(state);
+						token->len++;
+						depth++;
+					}
+				} else if (c == '*') {
+					next = peek(state);
+					if (next == '/') {
+						next_char(state);
+						token->len++;
+						depth--;
+					}
+				}
+			}
+			return;
+		}
+	}
+	if ('0' <= c && c <= '9') {
+		token->type = TOK_DIGITS;
+		ADD_WHILE('0' <= c && c <= '9');
+		return;
+	}
+	if (is_symbol(c)) {
+		token->type = TOK_SYMBOL;
+		return;
+	}
+	if (is_bracket(c)) {
+		token->type = TOK_BRACKET;
+		return;
+	}
+	if (is_letter(c)) {
+		token->type = TOK_LETTERS;
+		ADD_WHILE(is_letter(c));
+		return;
+	}
+	if (c == '"') {
+		token->type = TOK_STRING;
+		while (true) {
+			c = peek(state);
+			if (c == -1) {
+				report_error_single_line(state, LEX_ERROR_EOF_IN_STRING,
+				                         token->location, state->location.column);
+				break;
+			}
+			if (c == '\n') {
+				report_error_single_line(state, LEX_ERROR_UNTERMINATED_STRING,
+				                         token->location, state->location.column);
+				break;
+			}
+			next_char(state);
+			token->len++;
+			if (c == '"') {
+				break;
+			}
+			if (c == '\\') {
+				c = peek(state);
+				if (c == '"') {
+					next_char(state);
+					token->len++;
+				}
+			}
+		}
+		return;
+	}
+	token->type = TOK_UNKNOWN;
 }
 
 
@@ -212,17 +400,26 @@ int format_main(int argc, char **argv) {
 	u32 cap = 4096;
 	u32 size = 0;
 	u8 *contents = malloc(cap);
+	u32 lines = 0;
 	while (true) {
 		u32 wanted = cap - size;
 		usize result = fread(contents + size, 1, wanted, file);
+		b32 done = false;
 		if (result != wanted) {
 			if (ferror(file)) {
 				die();
 			}
-			size += result;
-			break;
+			done = true;
+		}
+		for (u32 i = size; i < size + result; i++) {
+			if (contents[i] == '\n') {
+				lines++;
+			}
 		}
 		size += result;
+		if (done) {
+			break;
+		}
 		cap += cap >> 1;
 		contents = realloc(contents, cap);
 	}
@@ -232,21 +429,53 @@ int format_main(int argc, char **argv) {
 	state.file = contents;
 	state.len = size;
 	state.location.file = fname;
+	state.lines = malloc(lines * sizeof(u8 *));
 	Token token = { .type = TOK_UNKNOWN };
 	while (token.type != TOK_EOF) {
 		next_token(&state, &token);
-		printf("%s:%u:%u: (indent %d, has_align: %s) ",
-		       token.location.file, token.location.line, token.location.column,
-		       token.indent_level, token.has_alignment ? "true" : "false");
+		printf("%s:%u:%u: ", token.location.file, token.location.line, token.location.column);
 		switch (token.type) {
-		case TOK_WORD: printf("WORD \"%.*s\"", token.len, token.start); break;
-		case TOK_OPERATOR: printf("OPERATOR \"%.*s\"", token.len, token.start); break;
-		case TOK_BRACKET: printf("BRACKET '%c'", *token.start); break;
+		case TOK_LETTERS: printf("LETTERS \"%.*s\"", token.len, token.start); break;
+		case TOK_DIGITS: printf("DIGITS \"%.*s\"", token.len, token.start); break;
+		case TOK_SYMBOL: printf("SYMBOL '%.*s'", token.len, token.start); break;
+		case TOK_BRACKET: printf("BRACKET '%.*s'", token.len, token.start); break;
 		case TOK_STRING: printf("STRING \"%.*s\"", token.len, token.start); break;
+		case TOK_NEWLINE: printf("NEWLINE indent=%u", token.len - 1); break;
+		case TOK_WHITESPACE: printf("WHITESPACE \"%.*s\"", token.len, token.start); break;
+		case TOK_COMMENT: printf("COMMENT \"%.*s\"", token.len, token.start); break;
 		case TOK_UNKNOWN: printf("UNKNOWN \"%.*s\"", token.len, token.start); break;
 		case TOK_EOF: printf("EOF"); break;
 		}
 		printf("\n");
+	}
+	for (u32 i = 0; i < state.error_count; i++) {
+		LexerError *error = &state.errors[i];
+		if (error->location_start.line == error->location_end.line) {
+			fprintf(stderr, "%s:%d:%d",
+			        error->location_start.file,
+			        error->location_start.line,
+			        error->location_start.column);
+			if (error->location_start.column != error->location_end.column) {
+				fprintf(stderr, "-%d", error->location_end.column);
+			}
+			fprintf(stderr, ": %s\n", lexer_error_messages[error->type]);
+			u8 *line = state.lines[error->location_start.line - 1];
+			u8 *end = (u8 *) strchr((char *)line, '\n');
+			fprintf(stderr, "%.*s\n", (u32) (end - line), line);
+			for (u32 j = 1; j < error->location_start.column; j++) {
+				if (line[j - 1] == '\t') {
+					fprintf(stderr, "        ");
+				} else {
+					fputc(' ', stderr);
+				}
+			}
+			for (u32 j = error->location_start.column; j <= error->location_end.column; j++) {
+				fputc('^', stderr);
+			}
+			fputc('\n', stderr);
+		} else {
+			assert("Multi line errors not yet implemented" == 0);
+		}
 	}
 	return 0;
 }
